e829daf66be689512c46f9639e4c7ed488816faa
luozhiqiang <luozhiqiang@datacloak.com>
2024-05-21 21:36:15 +0800

    feat: 新增@eason-cli/log 模块

---
+++ packages/utils/log/README.md
@@ -0,0 +1,11 @@
+# `@eason-cli/log`
+
+> TODO: description
+
+## Usage
+
+```
+const log = require('@eason-cli/log');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/utils/log/lib/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const log = require("npmlog");
+
+log.level = process.env.LOG_LEVEL ? process.env.LOG_LEVEL : "info"; // 判断debug模式
+log.heading = "eason-cli"; // 修改前缀
+log.addLevel("success", 2000, { fg: "green", bold: true }); // 添加自定义命令及样式
+
+module.exports = log;
